0a00062127a0ac2a88b85f75cbb0ea56d4ed27a8
Dongwoon Son <dplusic@gmail.com>
2018-12-30 23:07:32 +0900

    Add -f (--no-history) flag to gh-pages

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ if [ ! -n "$GITHUB_TOKEN" ]; then
   echo "You need to supply GITHUB_TOKEN"
   exit 1
 fi
-gh-pages -d $PUBLIC_PATH -b gh-pages -u "github-actions-bot <support+actions@github.com>"
+gh-pages -d $PUBLIC_PATH -b gh-pages -u "github-actions-bot <support+actions@github.com>" -f
 retval=$?
 if [ $retval -ne 0 ]; then
   echo "gh-pages failed: $retval"
